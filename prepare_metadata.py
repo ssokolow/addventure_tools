64c88b8459c1e3f8d2efebdb014d265c0d8bca8a
Stephan Sokolow <http://www.ssokolow.com/ContactMe>
2016-09-04 04:49:44 -0400

    Commit initial proof-of-concept prepare_metadata.py

---
+++ prepare_metadata.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""Simple tool for generating prepared data from a JSON
+"list of records"-format data dump.
+"""
+
+# TODO: Remove this permanently once Flake8 allows ignoring E731 selectively
+# flake8: noqa
+
+from __future__ import (absolute_import, division, print_function,
+                        with_statement, unicode_literals)
+
+__author__ = "Stephan Sokolow (deitarion/SSokolow)"
+__appname__ = "JSON Data Transformation Helper"
+__version__ = "0.1"
+__license__ = "MIT"
+
+import json, logging
+from itertools import groupby
+
+log = logging.getLogger(__name__)
+
+class BadInputError(Exception):
+    """Raised when the user's requested action is incompatible with the data"""
+
+# pylint: disable=unnecessary-lambda
+def group_by_multiple(records, field_names, render_inner=lambda x: list(x)):
+    """Recursively call itertools.groupby() based on a list of field names.
+
+    @param keying_is_primary: If true, require that each innermost set of
+        results contain only a single record and don't wrap them in a list.
+    """
+    is_last = len(field_names) <= 1
+    for key, group in groupby(records, lambda rec: rec[field_names[0]]):
+        if is_last:
+            yield key, render_inner(group)
+        else:
+            yield key, dict(group_by_multiple(group, field_names[1:],
+                            render_inner))
+
+def records_as_ids(records):
+    """Generator to convert a list of records into a list of IDs."""
+    for record in records:
+        yield record['id']
+
+def render_inner_single(items):
+    """render_inner callback for --is-primary"""
+    items = list(items)
+    if len(items) != 1:
+        raise BadInputError("--is-primary specified but specified key matches "
+                            "more than one record: %r" % items)
+    return items[0]
+
+def key_by(records, args):
+    """The C{key-by} subcommand"""
+    if args.is_primary:
+        render_inner = render_inner_single
+    else:
+        render_inner = lambda x: list(x)  # noqa
+
+    result = group_by_multiple(records, args.key, render_inner)
+    print(json.dumps(dict(result), indent=2))
+
+def index_by(records, args):
+    """The C{index-by} subcommand"""
+    if args.is_primary:
+        render_inner = lambda x: render_inner_single(records_as_ids(x)) # noqa
+    else:
+        render_inner = lambda x: list(records_as_ids(x))  # noqa
+
+    result = group_by_multiple(records, args.key, render_inner)
+    print(json.dumps(dict(result), indent=2))
+
+# -- Code Here --
+
+def main():
+    """The main entry point, compatible with setuptools entry points."""
+    # If we're running on Python 2, take responsibility for preventing
+    # output from causing UnicodeEncodeErrors. (Done here so it should only
+    # happen when not being imported by some other program.)
+    import sys
+    if sys.version_info.major < 3:
+        reload(sys)
+        sys.setdefaultencoding('utf-8')  # pylint: disable=no-member
+
+    from argparse import ArgumentParser, RawTextHelpFormatter, FileType
+    parser = ArgumentParser(formatter_class=RawTextHelpFormatter,
+            description=__doc__.replace('\r\n', '\n').split('\n--snip--\n')[0])
+    parser.add_argument('--version', action='version',
+            version="%%(prog)s v%s" % __version__)
+    parser.add_argument('-v', '--verbose', action="count",
+        default=2, help="Increase the verbosity. Use twice for extra effect")
+    parser.add_argument('-q', '--quiet', action="count",
+        default=0, help="Decrease the verbosity. Use twice for extra effect")
+    parser.add_argument('-i', '--infile', action="store", type=FileType('r'),
+                        default="./addventure_meta.json",
+                        help="Specify the JSON file to read from "
+                        "(default: %(default)s, use '-' for stdin)")
+
+    subparsers = parser.add_subparsers()
+
+    parser_key_by = subparsers.add_parser('key-by', help='Produce a (possibly '
+        'nested) dict, mapping keys to record data.')
+    parser_key_by.add_argument('key', nargs='+')
+    parser_key_by.add_argument('--is-primary', action="store_true",
+        default=False, help="require that the specified set of keys uniquely "
+        "identify records and don't wrap the records in a list.")
+    parser_key_by.set_defaults(func=key_by)
+
+    parser_index_by = subparsers.add_parser('index-by', help='Produce a '
+        '(possibly nested) dict, mapping keys to record IDs.')
+    parser_index_by.add_argument('key', nargs='+')
+    parser_index_by.add_argument('--is-primary', action="store_true",
+        default=False, help="Require that the specified set of keys uniquely "
+        "identify records and don't wrap the records in a list.")
+    parser_index_by.set_defaults(func=index_by)
+
+    args = parser.parse_args()
+
+    # Set up clean logging to stderr
+    log_levels = [logging.CRITICAL, logging.ERROR, logging.WARNING,
+                  logging.INFO, logging.DEBUG]
+    args.verbose = min(args.verbose - args.quiet, len(log_levels) - 1)
+    args.verbose = max(args.verbose, 0)
+    logging.basicConfig(level=log_levels[args.verbose],
+                        format='%(levelname)s: %(message)s')
+
+    records = json.load(args.infile)
+    log.debug("Loaded %d records", len(records))
+
+    #records = records[:20]  # TODO: Remove this test line
+    args.func(records, args)
+
+if __name__ == '__main__':
+    main()
+
+# vim: set sw=4 sts=4 expandtab :
